2945e4f2c8b7b219a6a8cb2de20c19d1e8c60c84
nikita-py <58150644+nikita-py@users.noreply.github.com>
2020-03-12 16:51:14 +0200

    Add files via upload

---
+++ js/app.js
@@ -0,0 +1,16 @@
+ReactDOM.render(
+	React.createElement('h1',null,'Hello,world!'),
+	document.getElementById('root')
+);
+var App = React.createClass({
+	render: function() {
+        return (
+		  Hello   
+		);
+	}
+})
+
+ReactDOM.render( 
+    <App />,
+    document.getElementById('root')
+);
